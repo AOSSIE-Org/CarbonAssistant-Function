4f8e36f07535a58ce74005d39411559262458f71
Varun Chitre <varun.chitre15@gmail.com>
2018-05-25 00:46:54 +0530

    carbon-function: add support for electricity consumption queries
    Signed-off-by: Varun Chitre <varun.chitre15@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -41,6 +41,87 @@ function processV1Request(request, response) {
                 sendResponse('Hello, Welcome to my Dialogflow agent!'); // Send simple response to user
             }
         },
+        'input.electricity_details': () => {
+            let consumed_quantity = 1,
+                consumption_country = 'Default',
+                emission_type = '';
+
+            if (parameters.quantity !== "")
+                consumed_quantity = parameters.quantity;
+
+            if (parameters.geo_country !== "")
+                consumption_country = parameters.geo_country;
+
+            console.log("Electricity consumed =" + consumed_quantity + ", country =" + consumption_country);
+
+            var options = {
+                uri: config.endpoint + "/emissions",
+                method: 'POST',
+                headers: {
+                    'access-key': config.access_key
+                },
+                json: true,
+                body: {
+                    "item": "electricity",
+                    "unit": "kWh",
+                    "region": consumption_country,
+                    "quantity": consumed_quantity
+                }
+            };
+
+            requestLib(options, function(error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    console.log(body);
+                    if (parameters.emission_type !== "") {
+                        let emissionType = parameters.emission_type;
+                        let emission;
+                        if (emissionType === "CO2")
+                            emission = body.emissions.CO2;
+                        else if (emissionType === "N2O")
+                            emission = body.emissions.N2O;
+                        else
+                            emission = body.emissions.CH4;
+
+                        let basicResponseString = emissionType + ' emission for electricity consumption of ' + consumed_quantity + ' kWh';
+                        let finalResponseString = "";
+
+                        if (consumption_country != "" && consumption_country != "Default")
+                            finalResponseString = basicResponseString + ' in ' + consumption_country + ' is ' + emission;
+                        else
+                            finalResponseString = basicResponseString + ' is ' + emission;
+
+
+                        let unit = body.unit;
+                        if (unit !== undefined)
+                            sendGoogleResponse(finalResponseString + ' ' + unit);
+                        else
+                            sendGoogleResponse(emissionType + ' emission for a ' + appliance_size + ' ' + appliance_type + ' ' + parameters.appliance + ' in ' + parameters.geo_country + ' is ' + emission + ' kg');
+                    } else {
+                        let basicResponseString = 'Emissions for electricity consumption of ' + consumed_quantity + ' kWh';
+                        let finalResponseString = "";
+                        if (consumption_country != "" && consumption_country != "Default")
+                            finalResponseString = basicResponseString + ' in ' + consumption_country;
+                        else
+                            finalResponseString = basicResponseString;
+                        let carbonEmission = body.emissions.CO2;
+                        let nitrousEmission = body.emissions.N2O;
+                        let methaneEmission = body.emissions.CH4;
+                        sendGoogleResponse(finalResponseString + ' are as follows:\n  \n' +
+                            'Carbon Dioxide: ' + carbonEmission + ' kg.\n' +
+                            "Nitrous Oxide: " + nitrousEmission + ' kg.\n' +
+                            "Methane: " + methaneEmission + ' kg.');
+                    }
+                } else {
+                    if (body.err !== undefined) {
+                        console.log("Error: " + JSON.stringify(body));
+                        sendGoogleResponse(body.err);
+                    } else {
+                        sendGoogleResponse("Sorry, we are facing a temporary outage. Please contact our support.");
+                    }
+                }
+            });
+
+        },
         'input.poultry_details': () => {
             if (parameters.poultry_type !== "") {
                 let poultry_type = parameters.poultry_type;
