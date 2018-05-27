f107e59bdf04422a8893dce36268f549d924602e
Varun Chitre <varun.chitre15@gmail.com>
2018-05-27 18:08:32 +0530

    carbon-function: add support for fuel consumption queries
    Signed-off-by: Varun Chitre <varun.chitre15@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -41,6 +41,82 @@ function processV1Request(request, response) {
                 sendResponse('Hello, Welcome to my Dialogflow agent!'); // Send simple response to user
             }
         },
+        'input.fuel_details': () => {
+            if (parameters.fuel_type != "" && parameters.fuel_original !== "") {
+                let consumed_quantity = 1,
+                    consumption_unit = 'Litre(s)',
+                    consumed_fuel_type = parameters.fuel_type,
+                    consumed_fuel_original = parameters.fuel_original;
+
+                if (parameters.quantity !== "")
+                    consumed_quantity = parameters.quantity;
+
+                if (parameters.fuel_type === "fuelCNG")
+                    consumption_unit = 'kg';
+
+                var options = {
+                    uri: config.endpoint + "/emissions",
+                    method: 'POST',
+                    headers: {
+                        'access-key': config.access_key
+                    },
+                    json: true,
+                    body: {
+                        "item": consumed_fuel_type,
+                        "unit": consumption_unit,
+                        "quantity": consumed_quantity
+                    }
+                };
+
+                requestLib(options, function(error, response, body) {
+                    if (!error && response.statusCode === 200) {
+                        console.log(body);
+                        if (parameters.emission_type !== "") {
+                            let emissionType = parameters.emission_type;
+                            let emission;
+                            if (emissionType === "CO2")
+                                emission = body.emissions.CO2;
+                            else if (emissionType === "N2O")
+                                emission = body.emissions.N2O;
+                            else
+                                emission = body.emissions.CH4;
+
+                            let basicResponseString = emissionType + ' emission for ' + consumed_quantity + ' ' + consumption_unit + ' ' +
+                                consumed_fuel_original + ' consumption is ' + emission;
+                            let finalResponseString = basicResponseString;
+
+
+                            let unit = body.unit;
+                            if (unit !== undefined)
+                                sendGoogleResponse(finalResponseString + ' ' + unit);
+                            else
+                                sendGoogleResponse(finalResponseString + ' kg');
+                        } else {
+                            let basicResponseString = 'Emissions for ' + consumed_quantity + ' ' + consumption_unit + ' ' +
+                                consumed_fuel_original + ' consumption';
+                            let finalResponseString = basicResponseString;
+
+                            let carbonEmission = body.emissions.CO2;
+                            let nitrousEmission = body.emissions.N2O;
+                            let methaneEmission = body.emissions.CH4;
+                            sendGoogleResponse(finalResponseString + ' are as follows:\n  \n' +
+                                'Carbon Dioxide: ' + carbonEmission + ' kg.\n' +
+                                "Nitrous Oxide: " + nitrousEmission + ' kg.\n' +
+                                "Methane: " + methaneEmission + ' kg.');
+                        }
+                    } else {
+                        if (body.err !== undefined) {
+                            console.log("Error: " + JSON.stringify(body));
+                            sendGoogleResponse(body.err);
+                        } else {
+                            sendGoogleResponse("Sorry, we are facing a temporary outage. Please contact our support.");
+                        }
+                    }
+                });
+            } else {
+                sendGoogleResponse("Sorry, I did not understand the fuel type you said.");
+            }
+        },
         'input.electricity_details': () => {
             let consumed_quantity = 1,
                 consumption_country = 'Default',
