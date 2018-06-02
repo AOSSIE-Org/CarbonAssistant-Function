ce8bcb3fca8b4c8aa7cb63a8c8716388c010d975
Varun Chitre <varun.chitre15@gmail.com>
2018-06-02 20:04:49 +0530

    carbon-function: add support for vehicle road trip emission queries
    Signed-off-by: Varun Chitre <varun.chitre15@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -39,6 +39,90 @@ function processV1Request(request, response) {
                 sendResponse('Hello, Welcome to my Dialogflow agent!'); // Send simple response to user
             }
         },
+        'input.vehicle_details': () => {
+            if (parameters.origin != "" && parameters.destination !== "" && parameters.fuel_type !== "") {
+                let origin = parameters.origin,
+                    destination = parameters.destination,
+                    fuel_type = getFuelType(parameters.fuel_type),
+                    mileage = 20;
+
+                if (parameters.mileage !== "")
+                    mileage = parameters.mileage;
+
+                var options = {
+                    uri: config.endpoint + "/vehicle",
+                    method: 'POST',
+                    headers: {
+                        'access-key': config.access_key
+                    },
+                    json: true,
+                    body: {
+                        "type": fuel_type,
+                        "origin": origin,
+                        "destination": destination,
+                        "mileage": mileage
+                    }
+                };
+
+                requestLib(options, function(error, response, body) {
+                    if (!error && response.statusCode === 200) {
+                        console.log(body);
+                        if (parameters.emission_type !== "") {
+                            let emissionType = parameters.emission_type;
+                            let emission;
+                            if (emissionType === "CO2")
+                                emission = body.emissions.CO2;
+                            else if (emissionType === "N2O")
+                                emission = body.emissions.N2O;
+                            else
+                                emission = body.emissions.CH4;
+
+                            let basicResponseString = emissionType + ' emissions for a road trip from ' + origin + ' to ' + destination + ' on a ' +
+                                fuel_type + '-based vehicle ';
+
+                            let finalResponseString;
+                            if (mileage !== 20)
+                                finalResponseString = basicResponseString + 'with a mileage of ' + mileage + ' km/l is ' + emission;
+                            else
+                                finalResponseString = basicResponseString + 'usually with a mileage of 20 km/l is ' + emission;
+
+
+                            let unit = body.unit;
+                            if (unit !== undefined)
+                                sendGoogleResponse(finalResponseString + ' ' + unit);
+                            else
+                                sendGoogleResponse(finalResponseString + ' kg');
+                        } else {
+                            let basicResponseString = 'Emissions for a road trip from ' + origin + ' to ' + destination + ' on a ' +
+                                fuel_type + '-based vehicle ';
+
+                            let finalResponseString;
+                            if (mileage !== 20)
+                                finalResponseString = basicResponseString + 'with a mileage of ' + mileage + ' km/l';
+                            else
+                                finalResponseString = basicResponseString + 'usually with a mileage of 20 km/l';
+
+                            let carbonEmission = body.emissions.CO2;
+                            let nitrousEmission = body.emissions.N2O;
+                            let methaneEmission = body.emissions.CH4;
+                            sendGoogleResponse(finalResponseString + ' are as follows:\n  \n' +
+                                'Carbon Dioxide: ' + carbonEmission + ' kg.\n' +
+                                "Nitrous Oxide: " + nitrousEmission + ' kg.\n' +
+                                "Methane: " + methaneEmission + ' kg.');
+                        }
+                    } else {
+                        if (body.err !== undefined) {
+                            console.log("Error: " + JSON.stringify(body));
+                            sendGoogleResponse(body.err);
+                        } else {
+                            sendGoogleResponse("Sorry, we are facing a temporary outage. Please contact our support.");
+                        }
+                    }
+                });
+            } else {
+                sendGoogleResponse("Sorry, need a valid origin, destination and type of fuel your vehicle uses. Could you please say it again?");
+            }
+        },
         'input.flight_details': () => {
             if (parameters.origin != "" && parameters.destination !== "") {
                 let origin = parameters.origin,
@@ -525,5 +609,9 @@ function processV1Request(request, response) {
             return duration.amount / (60 * 60);
         else
             return -1;
+    }
+
+    function getFuelType(fuelString) {
+        return fuelString.substring(4);
     }
 }
