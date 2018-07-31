7c8c50bbe5fbe6812c2e8dd26f08fda91916c937
Varun Chitre <varun.chitre15@gmail.com>
2018-07-31 17:28:58 +0530

    carbon-function: add a helper function to produce richResponses
    Signed-off-by: Varun Chitre <varun.chitre15@gmail.com>

--- functions/utils.js
+++ functions/utils.js
@@ -1,3 +1,7 @@
+const {
+    SimpleResponse
+} = require('actions-on-google');
+
 exports.roundWithPrecision = (value, precision) => {
     var multiplier = Math.pow(10, precision || 0);
     return Math.round(value * multiplier) / multiplier;
@@ -5,4 +9,11 @@ exports.roundWithPrecision = (value, precision) => {
 
 exports.getRandomNumber = (minimum, maximum) => {
     return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+}
+
+exports.richResponse = (conv, display, toSpeak) => {
+    conv.ask(new SimpleResponse({
+        speech: toSpeak,
+        text: display
+    }));
 }
