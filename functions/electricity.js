cc4abc75da31f6282839c154c6b1efef3fd9ba9e
siwaniagrawal <siwaniagrawal18@gmail.com>
2019-03-24 21:48:23 +0530

    Electricity: context management support

--- functions/electricity.js
+++ functions/electricity.js
@@ -1,12 +1,13 @@
 var config = require('./config')
 const requestLib = require('request');
 const utils = require('./utils');
+const reverseLookupManager = require('./reverseLookupManager');
 
-exports.processRequest = function(conv, parameters) {
+exports.processRequest = function(conv, parameters, requestReverseLookup) {
     return new Promise(function(resolve, reject) {
         let consumed_quantity = 1,
             consumption_country = 'Default',
-            emission_type = '';
+            emissionType = '';
 
         if (parameters.quantity !== "")
             consumed_quantity = parameters.quantity;
@@ -34,6 +35,7 @@ exports.processRequest = function(conv, parameters) {
         requestLib(options, function(error, response, body) {
             const emissionResponse = "The emissions released due to this action are given below";
             if (!error && response.statusCode === 200) {
+                //The response returned no error, as expected.
                 console.log(body);
                 if (parameters.emission_type !== "") {
                     let emissionType = parameters.emission_type;
@@ -44,23 +46,47 @@ exports.processRequest = function(conv, parameters) {
                         emission = body.emissions.N2O;
                     else
                         emission = body.emissions.CH4;
-
-                    let basicResponseString = emissionType + ' emissions due to electricity consumption of ' + consumed_quantity + ' kWh';
+                    let basicResponseString = emissionType +' emissions due to electricity consumption of ' + consumed_quantity + ' kWh';
                     let finalResponseString = "";
-
                     if (consumption_country != "" && consumption_country != "Default")
                         finalResponseString = basicResponseString + ' in ' + consumption_country + ' are ' + emission;
                     else
                         finalResponseString = basicResponseString + ' are ' + emission;
-
-
-                    let unit = body.unit;
-                    if (unit !== undefined) {
-                        utils.richResponse(conv, finalResponseString + ' ' + unit, emissionResponse);
-                        resolve();
+                    if (requestReverseLookup) {
+                        let reverseLookup = reverseLookupManager.reverseLookup(body.emissions, conv.user.storage.location.coordinates);
+                        reverseLookup
+                            .then((responses) => {
+                                let selectedResponse = utils.getRandomNumber(0, responses.length - 1);
+                                let unit = body.unit;
+                                if (unit !== undefined) {
+                                    finalResponseString = finalResponseString + ' ' + unit + ' \n\n' + responses[selectedResponse];
+                                    utils.richResponse(conv, finalResponseString, responses[selectedResponse]);
+                                    resolve();
+                                } else {
+                                    finalResponseString = finalResponseString + ' kg' + ' \n\n' + responses[selectedResponse];
+                                    utils.richResponse(conv, finalResponseString, responses[selectedResponse]);
+                                    resolve();
+                                }
+                            })
+                            .catch((err) => {
+                                let unit = body.unit;
+                                if (unit !== undefined) {
+                                    utils.richResponse(conv, finalResponseString + ' ' + unit, emissionResponse);
+                                    resolve();
+                                } else {
+                                    utils.richResponse(conv, finalResponseString + ' kg', emissionResponse);
+                                    resolve();
+                                }
+                            });
                     } else {
-                        utils.richResponse(conv, finalResponseString + ' kg', emissionResponse);
-                        resolve();
+                        let unit = body.unit;
+                        if (unit !== undefined) {
+                            utils.richResponse(conv, finalResponseString + ' ' + unit, emissionResponse);
+                            resolve();
+                        } else {
+                            utils.richResponse(conv, finalResponseString + ' kg', emissionResponse);
+                            resolve();
+                        }
                     }
                 } else {
                     let basicResponseString = 'Emissions due to electricity consumption of ' + consumed_quantity + ' kWh';
@@ -72,15 +98,44 @@ exports.processRequest = function(conv, parameters) {
                     let carbonEmission = body.emissions.CO2;
                     let nitrousEmission = body.emissions.N2O;
                     let methaneEmission = body.emissions.CH4;
-                    finalResponseString = finalResponseString + ' are as follows:\n  \n' +
-                        'Carbon Dioxide: ' + carbonEmission + ' kg.\n' +
-                        "Nitrous Oxide: " + nitrousEmission + ' kg.\n' +
-                        "Methane: " + methaneEmission + ' kg.';
-                    utils.richResponse(conv, finalResponseString, emissionResponse);
-                    resolve();
+
+                    if (requestReverseLookup) {
+                        console.log("Location data:" + JSON.stringify(conv.user.storage.location.coordinates));
+                        let reverseLookup = reverseLookupManager.reverseLookup(body.emissions, conv.user.storage.location.coordinates);
+                        reverseLookup
+                            .then((responses) => {
+                                console.log("responses length: " + responses.length);
+                                let selectedResponse = utils.getRandomNumber(0, responses.length - 1);
+                                console.log("selected response: " + selectedResponse);
+                                finalResponseString = finalResponseString + ' are as follows:\n  \n' +
+                                    'Carbon Dioxide: ' + carbonEmission + ' kg.\n' +
+                                    "Nitrous Oxide: " + nitrousEmission + ' kg.\n' +
+                                    "Methane: " + methaneEmission + ' kg.' + ' \n\n' + responses[selectedResponse];
+                                utils.richResponse(conv, finalResponseString, responses[selectedResponse]);
+                                resolve();
+                            })
+                            .catch((err) => {
+                                finalResponseString = finalResponseString + ' are as follows:\n  \n' +
+                                    'Carbon Dioxide: ' + carbonEmission + ' kg.\n' +
+                                    "Nitrous Oxide: " + nitrousEmission + ' kg.\n' +
+                                    "Methane: " + methaneEmission + ' kg.';
+                                utils.richResponse(conv, finalResponseString, emissionResponse);
+                                resolve();
+                            });
+                    } else {
+                        finalResponseString = finalResponseString + ' are as follows:\n  \n' +
+                            'Carbon Dioxide: ' + carbonEmission + ' kg.\n' +
+                            "Nitrous Oxide: " + nitrousEmission + ' kg.\n' +
+                            "Methane: " + methaneEmission + ' kg.';
+                        utils.richResponse(conv, finalResponseString, emissionResponse);
+                        resolve();
+                    }
                 }
             } else {
+              
               //Handle the error in the utils function
+              if (!error)
+                error = body.error;
               utils.handleError(error, response, body, conv);
               resolve();
             }
