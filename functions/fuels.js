93865341d52bf13ed55b7ef28d562a15960863a4
siwaniagrawal <siwaniagrawal18@gmail.com>
2019-03-25 09:00:29 +0530

    Fuels: support for context management added

--- functions/fuels.js
+++ functions/fuels.js
@@ -1,8 +1,9 @@
 var config = require('./config')
 const requestLib = require('request');
 const utils = require('./utils');
+const reverseLookupManager = require('./reverseLookupManager');
 
-exports.processRequest = function(conv, parameters) {
+exports.processRequest = function(conv, parameters, requestReverseLookup) {
     return new Promise(function(resolve, reject) {
         if (parameters.fuel_type != "" && parameters.fuel_original !== "") {
             let consumed_quantity = 1,
@@ -44,35 +45,84 @@ exports.processRequest = function(conv, parameters) {
                         else
                             emission = body.emissions.CH4;
 
-                        let basicResponseString = emissionType + ' emissions due to ' + consumed_quantity + ' ' + consumption_unit + ' ' +
+                        let basicResponseString = emissionType + ' emissions due to ' + consumed_quantity + ' ' + consumption_unit + ' of ' +
                             consumed_fuel_original + ' consumption are ' + emission;
                         let finalResponseString = basicResponseString;
-
-
-                        let unit = body.unit;
-                        if (unit !== undefined) {
-                            finalResponseString = finalResponseString + ' ' + unit;
-                            utils.richResponse(conv, finalResponseString, emissionResponse);
-                            resolve();
+                        if (requestReverseLookup) {
+                            let reverseLookup = reverseLookupManager.reverseLookup(body.emissions, conv.user.storage.location.coordinates);
+                            reverseLookup
+                                .then((responses) => {
+                                    let selectedResponse = utils.getRandomNumber(0, responses.length - 1);
+                                    let unit = body.unit;
+                                    if (unit !== undefined) {
+                                        finalResponseString = finalResponseString + ' ' + unit + ' \n\n' + responses[selectedResponse];
+                                        utils.richResponse(conv, finalResponseString, responses[selectedResponse]);
+                                        resolve();
+                                    } else {
+                                        finalResponseString = finalResponseString + ' kg' + ' \n\n' + responses[selectedResponse];
+                                        utils.richResponse(conv, finalResponseString, responses[selectedResponse]);
+                                        resolve();
+                                    }
+                                })
+                                .catch((err) => {
+                                    let unit = body.unit;
+                                    if (unit !== undefined) {
+                                        utils.richResponse(conv, finalResponseString + ' ' + unit, emissionResponse);
+                                        resolve();
+                                    } else {
+                                        utils.richResponse(conv, finalResponseString + ' kg', emissionResponse);
+                                        resolve();
+                                    }
+                                });
                         } else {
-                            finalResponseString = finalResponseString + ' kg';
-                            utils.richResponse(conv, finalResponseString, emissionResponse);
-                            resolve();
+                            let unit = body.unit;
+                            if (unit !== undefined) {
+                                utils.richResponse(conv, finalResponseString + ' ' + unit, emissionResponse);
+                                resolve();
+                            } else {
+                                utils.richResponse(conv, finalResponseString + ' kg', emissionResponse);
+                                resolve();
+                            }
                         }
                     } else {
-                        let basicResponseString = 'Emissions due to ' + consumed_quantity + ' ' + consumption_unit + ' ' +
+                        let basicResponseString = 'Emissions due to ' + consumed_quantity + ' ' + consumption_unit + ' of ' +
                             consumed_fuel_original + ' consumption';
                         let finalResponseString = basicResponseString;
 
                         let carbonEmission = body.emissions.CO2;
                         let nitrousEmission = body.emissions.N2O;
                         let methaneEmission = body.emissions.CH4;
-                        finalResponseString = finalResponseString + ' are as follows:\n  \n' +
-                            'Carbon Dioxide: ' + carbonEmission + ' kg.\n' +
-                            "Nitrous Oxide: " + nitrousEmission + ' kg.\n' +
-                            "Methane: " + methaneEmission + ' kg.'
-                        utils.richResponse(conv, finalResponseString, emissionResponse);
-                        resolve();
+                        if (requestReverseLookup) {
+                            console.log("Location data:" + JSON.stringify(conv.user.storage.location.coordinates));
+                            let reverseLookup = reverseLookupManager.reverseLookup(body.emissions, conv.user.storage.location.coordinates);
+                            reverseLookup
+                                .then((responses) => {
+                                    console.log("responses length: " + responses.length);
+                                    let selectedResponse = utils.getRandomNumber(0, responses.length - 1);
+                                    console.log("selected response: " + selectedResponse);
+                                    finalResponseString = finalResponseString + ' are as follows:\n  \n' +
+                                        'Carbon Dioxide: ' + carbonEmission + ' kg.\n' +
+                                        "Nitrous Oxide: " + nitrousEmission + ' kg.\n' +
+                                        "Methane: " + methaneEmission + ' kg.' + ' \n\n' + responses[selectedResponse];
+                                    utils.richResponse(conv, finalResponseString, responses[selectedResponse]);
+                                    resolve();
+                                })
+                                .catch((err) => {
+                                    finalResponseString = finalResponseString + ' are as follows:\n  \n' +
+                                        'Carbon Dioxide: ' + carbonEmission + ' kg.\n' +
+                                        "Nitrous Oxide: " + nitrousEmission + ' kg.\n' +
+                                        "Methane: " + methaneEmission + ' kg.';
+                                    utils.richResponse(conv, finalResponseString, emissionResponse);
+                                    resolve();
+                                });
+                        } else {
+                            finalResponseString = finalResponseString + ' are as follows:\n  \n' +
+                                'Carbon Dioxide: ' + carbonEmission + ' kg.\n' +
+                                "Nitrous Oxide: " + nitrousEmission + ' kg.\n' +
+                                "Methane: " + methaneEmission + ' kg.';
+                            utils.richResponse(conv, finalResponseString, emissionResponse);
+                            resolve();
+                        }
                     }
                 } else {
                   //Handle the error in the utils function
