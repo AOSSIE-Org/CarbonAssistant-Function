4bcfd86e6bd1fd3305459de754186b92923013df
Siwani Agrawal <siwaniagrawal18@gmail.com>
2019-06-11 18:07:59 +0000

    Wizard-Support for Land related emission queries.

---
+++ functions/land_utils.js
@@ -0,0 +1,21 @@
+const landtypes = require('./assets/raw_data_land.json');
+const Image =require('actions-on-google').Image;
+
+exports.getLandTypes = ()=>{
+    var items = {};
+    var data = (landtypes);
+    console.log("data: ", data);
+    data.forEach((obj) => {
+        items[obj.land]= {
+            title: obj.land,
+            description: obj.description,
+            image: new Image({
+              url: obj.url,
+              alt: obj.alt,
+            }),
+        }
+        
+        
+    });
+    return items;
+   }
