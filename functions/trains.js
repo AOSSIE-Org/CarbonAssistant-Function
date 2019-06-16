864a48d15b17e1ad67b9ccdf6ff9a460b0d87c42
Siwani Agrawal <siwaniagrawal18@gmail.com>
2019-06-16 15:29:09 +0000

    Error handling of the intents' JS files

--- functions/trains.js
+++ functions/trains.js
@@ -61,6 +61,9 @@ exports.processRequest = function(conv, parameters, requestReverseLookup) {
                         resolve();
                     }
                 } else {
+                  // Handle errors here
+                  if (!error)
+                    error = body.error;
                   //Handle the error in the utils function
                   utils.handleError(error, response, body, conv);
                   resolve();
